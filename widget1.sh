3a304d0780821fd85e39cfb151b470c7c546373a
Randomlv01 <thees@neuhaus.rocks>
2025-11-02 17:37:26 +0100

    Enhance widget1.sh with additional system metrics
    Added memory, temperature, and WiFi signal information to the status bar.

--- widget1.sh
+++ widget1.sh
@@ -10,22 +10,28 @@ while true; do
     white=$'\e[0m'    
 
     # CPU-Auslastung
-    CPU=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *$[0-9.]*$%* id.*/\1/" | awk '{print 100 - $1}')
+  #  CPU=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *$[0-9.]*$%* id.*/\1/" | awk '{print 100 - $1}')
 
     # RAM-Nutzung
-    RAM=$(free -h | grep "Mem" | awk '{print $3 "/" $2}')
+  #  RAM=$(free -h | grep "Mem" | awk '{print $3 "/" $2}')
 
     # Festplattenspeicher
     DISK=$(df -h | grep "/dev/sda1" | awk '{print $3 "/" $2}')  # Hier möglicherweise anpassen
     
     # Uhrzeit
     TIME=$(date +"%H:%M")
-    
+
+
+            MEM=$(free -h --kilo | awk '/^Mem:/ {print $3 "/" $2}')
+        CPU=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}' )
+        TEMP=$(sensors|grep 'Core 0'|awk '{print $3}' )
+        WIFI=$(nmcli -f ACTIVE,SIGNAL dev wifi list | awk '$1=="yes" {print $2}')
+
     # Lautstärke
     VOLUME=$(amixer get Master | grep -o "[0-9]*%" | head -n 1)
 
     # Setzen der xsetroot-Anzeige
-    xsetroot -name "${red}DISK: ${DISK}${white} | ${cyan}TIME: ${TIME}${white} | ${magenta}VOLUME: ${VOLUME}"
+    xsetroot -name "${red}DISK: ${DISK}${white} | ${cyan}TIME: ${TIME}${white} | ${magenta}VOLUME: ${VOLUME}${white} | ${green}$TEMP "
 
     sleep 0.1  # Aktualisierungsintervall
 done
